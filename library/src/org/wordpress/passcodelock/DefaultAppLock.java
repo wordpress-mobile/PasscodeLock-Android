7daa8355fd34fb12361268ec67836e4ffff59aac
Maxime Biais <maxime.biais@gmail.com>
2014-06-10 16:57:27 +0800

    replace Config.java by BuildConfig.java

--- library/src/org/wordpress/passcodelock/DefaultAppLock.java
+++ library/src/org/wordpress/passcodelock/DefaultAppLock.java
@@ -16,19 +16,21 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Base64;
 
+import org.wordpress.passcodelock.BuildConfig;
+
 public class DefaultAppLock extends AbstractAppLock {
 
     private Application currentApp; //Keep a reference to the app that invoked the locker
     private SharedPreferences settings;
     private Date lostFocusDate;
-    
-    //Add back-compatibility 
+
+    //Add back-compatibility
     private static final String OLD_PASSWORD_SALT = "sadasauidhsuyeuihdahdiauhs";
     private static final String OLD_APP_LOCK_PASSWORD_PREF_KEY = "wp_app_lock_password_key";
-    
+
     public DefaultAppLock(Application currentApp) {
         super();
-        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(currentApp); 
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(currentApp);
         this.settings = settings;
         this.currentApp = currentApp;
     }
@@ -36,39 +38,39 @@ public class DefaultAppLock extends AbstractAppLock {
     public void enable(){
     	if (android.os.Build.VERSION.SDK_INT < 14)
     		return;
-    	  
+
         if( isPasswordLocked() ) {
             currentApp.unregisterActivityLifecycleCallbacks(this);
             currentApp.registerActivityLifecycleCallbacks(this);
         }
     }
-    
+
     public void disable( ){
     	if (android.os.Build.VERSION.SDK_INT < 14)
     		return;
-    	
+
         currentApp.unregisterActivityLifecycleCallbacks(this);
     }
-    
+
     public void forcePasswordLock(){
         lostFocusDate = null;
     }
-    
+
     public boolean verifyPassword( String password ){
     	String storedPassword = "";
-    	 
-    	if (settings.contains(OLD_APP_LOCK_PASSWORD_PREF_KEY)) { //add back-compatibility 
+
+    	if (settings.contains(OLD_APP_LOCK_PASSWORD_PREF_KEY)) { //add back-compatibility
     		//Check if the old value is available
     		storedPassword = settings.getString(OLD_APP_LOCK_PASSWORD_PREF_KEY, "");
     		password = OLD_PASSWORD_SALT + password + OLD_PASSWORD_SALT;
             password = StringUtils.getMd5Hash(password);
-    	} else if (settings.contains(Config.PASSWORD_PREFERECENCE_KEY)) {
-    		//read the password from the new key 
-    		storedPassword = settings.getString(Config.PASSWORD_PREFERECENCE_KEY, "");
+    	} else if (settings.contains(BuildConfig.PASSWORD_PREFERENCE_KEY)) {
+    		//read the password from the new key
+    		storedPassword = settings.getString(BuildConfig.PASSWORD_PREFERENCE_KEY, "");
     		storedPassword = decryptPassword(storedPassword);
-    		password = Config.PASSWORD_SALT + password +  Config.PASSWORD_SALT;
+    		password = BuildConfig.PASSWORD_SALT + password +  BuildConfig.PASSWORD_SALT;
     	}
-        
+
         if( password.equalsIgnoreCase(storedPassword) ) {
             lostFocusDate = new Date();
             return true;
@@ -76,43 +78,43 @@ public class DefaultAppLock extends AbstractAppLock {
             return false;
         }
     }
-    
+
     public boolean setPassword(String password){
         SharedPreferences.Editor editor = settings.edit();
 
         if(password == null) {
             editor.remove(OLD_APP_LOCK_PASSWORD_PREF_KEY);
-            editor.remove(Config.PASSWORD_PREFERECENCE_KEY);
+            editor.remove(BuildConfig.PASSWORD_PREFERENCE_KEY);
             editor.commit();
             this.disable();
         } else {
-            password = Config.PASSWORD_SALT + password +  Config.PASSWORD_SALT;
+            password = BuildConfig.PASSWORD_SALT + password +  BuildConfig.PASSWORD_SALT;
             password = encryptPassword(password);
-            editor.putString(Config.PASSWORD_PREFERECENCE_KEY, password);
+            editor.putString(BuildConfig.PASSWORD_PREFERENCE_KEY, password);
             editor.remove(OLD_APP_LOCK_PASSWORD_PREF_KEY);
             editor.commit();
             this.enable();
         }
-        
+
         return true;
     }
-        
+
     //Check if we need to show the lock screen at startup
     public boolean isPasswordLocked(){
 
-    	if (settings.contains(OLD_APP_LOCK_PASSWORD_PREF_KEY)) //Check if the old value is available 
+    	if (settings.contains(OLD_APP_LOCK_PASSWORD_PREF_KEY)) //Check if the old value is available
     		return true;
 
-    	if (settings.contains(Config.PASSWORD_PREFERECENCE_KEY)) 
+    	if (settings.contains(BuildConfig.PASSWORD_PREFERENCE_KEY))
     		return true;
 
     	return false;
     }
-    
+
     private String encryptPassword(String clearText) {
         try {
             DESKeySpec keySpec = new DESKeySpec(
-                    Config.PASSWORD_ENC_SECRET.getBytes("UTF-8"));
+                    BuildConfig.PASSWORD_ENC_SECRET.getBytes("UTF-8"));
             SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
             SecretKey key = keyFactory.generateSecret(keySpec);
 
@@ -128,7 +130,7 @@ public class DefaultAppLock extends AbstractAppLock {
 
     private String decryptPassword(String encryptedPwd) {
         try {
-            DESKeySpec keySpec = new DESKeySpec(Config.PASSWORD_ENC_SECRET.getBytes("UTF-8"));
+            DESKeySpec keySpec = new DESKeySpec(BuildConfig.PASSWORD_ENC_SECRET.getBytes("UTF-8"));
             SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
             SecretKey key = keyFactory.generateSecret(keySpec);
 
@@ -141,23 +143,23 @@ public class DefaultAppLock extends AbstractAppLock {
         }
         return encryptedPwd;
     }
-    
+
     private boolean mustShowUnlockSceen() {
 
         if( isPasswordLocked() == false)
             return false;
-        
+
         if( lostFocusDate == null )
             return true; //first startup or when we forced to show the password
-        
+
         int currentTimeOut = lockTimeOut; //get a reference to the current password timeout and reset it to default
-        lockTimeOut = DEFAULT_TIMEOUT; 
+        lockTimeOut = DEFAULT_TIMEOUT;
         Date now = new Date();
         long now_ms = now.getTime();
         long lost_focus_ms = lostFocusDate.getTime();
         int secondsPassed = (int) (now_ms - lost_focus_ms)/(1000);
         secondsPassed = Math.abs(secondsPassed); //Make sure changing the clock on the device to a time in the past doesn't by-pass PIN Lock
-        if (secondsPassed >= currentTimeOut) {         
+        if (secondsPassed >= currentTimeOut) {
             lostFocusDate = null;
             return true;
         }
@@ -167,26 +169,26 @@ public class DefaultAppLock extends AbstractAppLock {
 
     @Override
     public void onActivityPaused(Activity arg0) {
-        
+
         if( arg0.getClass() == PasscodeUnlockActivity.class )
             return;
 
         if( ( this.appLockDisabledActivities != null ) && Arrays.asList(this.appLockDisabledActivities).contains( arg0.getClass().getName() ) )
      	   return;
-        
+
         lostFocusDate = new Date();
-        
+
     }
 
     @Override
     public void onActivityResumed(Activity arg0) {
-        
-        if( arg0.getClass() == PasscodeUnlockActivity.class ) 
+
+        if( arg0.getClass() == PasscodeUnlockActivity.class )
             return;
-        
+
        if(  ( this.appLockDisabledActivities != null ) && Arrays.asList(this.appLockDisabledActivities).contains( arg0.getClass().getName() ) )
     	   return;
-       
+
         if(mustShowUnlockSceen()) {
             //uhhh ohhh!
             Intent i = new Intent(arg0.getApplicationContext(), PasscodeUnlockActivity.class);
@@ -194,13 +196,13 @@ public class DefaultAppLock extends AbstractAppLock {
             arg0.getApplication().startActivity(i);
             return;
         }
-        
+
     }
 
     @Override
     public void onActivityCreated(Activity arg0, Bundle arg1) {
     }
-    
+
     @Override
     public void onActivityDestroyed(Activity arg0) {
     }
