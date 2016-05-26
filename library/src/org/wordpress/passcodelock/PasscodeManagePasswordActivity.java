bf2f0ededed4ff5a4de9b3c4dc26c802f0835a2e
Tyler Heck <theck13@hotmail.com>
2016-05-26 13:51:46 -0600

    Revert "Add screen shake to incorrect PIN for Manage"

--- library/src/org/wordpress/passcodelock/PasscodeManagePasswordActivity.java
+++ library/src/org/wordpress/passcodelock/PasscodeManagePasswordActivity.java
@@ -47,7 +47,7 @@ public class PasscodeManagePasswordActivity extends AbstractPasscodeKeyboardActi
                     AppLockManager.getInstance().getAppLock().setPassword(null);
                     authenticationSucceeded();
                 } else {
-                    authenticationFailed();
+                    showPasswordError();
                 }
                 break;
             case PasscodePreferenceFragment.ENABLE_PASSLOCK:
@@ -61,7 +61,7 @@ public class PasscodeManagePasswordActivity extends AbstractPasscodeKeyboardActi
                     } else {
                         unverifiedPasscode = null;
                         topMessage.setText(R.string.passcodelock_prompt_message);
-                        authenticationFailed();
+                        showPasswordError();
                     }
                 }
                 break;
@@ -71,7 +71,7 @@ public class PasscodeManagePasswordActivity extends AbstractPasscodeKeyboardActi
                     topMessage.setText(R.string.passcodelock_prompt_message);
                     type = PasscodePreferenceFragment.ENABLE_PASSLOCK;
                 } else {
-                    authenticationFailed();
+                    showPasswordError();
                 } 
                 break;
             default:
